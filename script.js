527ed913612d76d7f9da53e89141b5a08daba4a4
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-02-23 23:49:23 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let body = document.querySelector("body")
+let divContainer = document.createElement("div");
+let number1 = document.createElement("div");
+let number2 = document.createElement("div");
+let number3 = document.createElement("div");
+let number4 = document.createElement("div");
+let number5 = document.createElement("div");
+let number6 = document.createElement("div");
+let number7 = document.createElement("div");
+let numberP1 = document.createElement("p");
+let numberP2 = document.createElement("p");
+let numberP3 = document.createElement("p");
+let numberP4 = document.createElement("p");
+let numberP5 = document.createElement("p");
+let numberP6 = document.createElement("p");
+let numberP7 = document.createElement("p");
+divContainer.className="container";
+body.append(divContainer)
+number1.append(numberP1)
+number2.append(numberP2)
+number3.append(numberP3)
+number4.append(numberP4)
+number5.append(numberP5)
+number6.append(numberP6)
+number7.append(numberP7)
+number1.className="number";
+number2.className="number";
+number3.className="number";
+number4.className="number";
+number5.className="number";
+number6.className="number";
+number7.className="number";
+divContainer.append(number1,number2,number3,number4,number5,number6,number7)
